d211c3cda3e3e4b151260c366239ff3acc5c107a
yournameDebbie <d.eleyeem@gmail.com>
2024-03-14 22:30:01 -0700

    50% done PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,26 +22,37 @@ with open (csv_path, "r") as csvfile:
     #read file
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    next(csvreader, None)  # Skip the header
+
     #loop through data row by row
     for row in csvreader:
-        #count rows for month. Note this gives total of rows. Will need to subtract by one for headers later.
-        row_count += 1
+        #count rows for month. Note this gives total of rows.  
+        ######DOES LEN FUNCTION OF CSVREADER GET SAME OUTPUT?
+        tot_months += 1
 
-        for row in row[1]:
-            #add column [1] to itself
-            tot_PL = tot_PL + int(row)
+        #add column [1] to itself
+        tot_PL += int(row[1])
 
-tot_months = row_count - 1
+        #evaluate change in  PL??????
+
+        #
+        increase_max = max(row[1])
+        decrease_max = min(row[1])
+
+avg_PL = int(tot_PL)/int(tot_months)
+
 #save output path to write
 output_path = os.path.join("..", "Analysis", "PyBankAnalysis.csv")
 
 #open file to write
-with open(output_path, "w") as output_file:
+with open(output_path, "w", newline= '') as output_file:
 
     #initialize writer
     csvwriter = csv.writer(output_file, delimiter=",")
 
-    #write first row
-    csvwriter.writerow("Financial Analysis")
-    csvwriter.writerow("------------------------------")
-    csvwriter.writerow("Total Months: {tot_months}")
+    #write rows
+    #need to use brackets between ( and " to make it a tuple. The writer function expects iteration????
+    csvwriter.writerow(["Financial Analysis"])
+    csvwriter.writerow(["------------------------------"])
+    csvwriter.writerow([tot_months])
+ 
